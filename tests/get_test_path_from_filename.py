956fed742f7a769a1aff39c368e953cf804c9b23
MyNameIsTrez <welfje@gmail.com>
2023-01-06 20:24:16 +0100

    Use new test format

--- tests/get_test_path_from_filename.py
+++ tests/get_test_path_from_filename.py
@@ -1,2 +1,2 @@
 def get_test_path_from_filename(filename):
-    return f"tests/ini_test_files/token_and_cst/{filename}.ini"
+    return f"tests/ini_test_files/token_and_cst/{filename}/in.ini"
--- tests/test_ini_conversion.py
+++ tests/test_ini_conversion.py
@@ -10,19 +10,28 @@ from cortex_command_mod_converter_engine.ini_converting import (
 
 
 class TestINIConversion(unittest.TestCase):
-    CONVERSION_TESTS_PATH = Path("tests/ini_test_files/conversion")
+    CONVERSION_TESTS_PATH = Path("tests/ini_test_files")
 
     def test_conversions(self):
         for in_filepath, out_filepath in self.get_filepaths():
-            in_str = in_filepath.read_text()
-            out_str = out_filepath.read_text()
+            with self.subTest(msg=in_filepath):
+                in_str = in_filepath.read_text()
+                out_str = out_filepath.read_text()
 
-            tokens = ini_tokenizer.get_tokens_from_str(in_str, in_filepath)
-            cst = ini_cst.get_cst(tokens)
-            ini_rules.apply_rules_on_sections([cst[0]], None)
+                tokens = ini_tokenizer.get_tokens_from_str(in_str.lstrip(), in_filepath)
 
-            result = ini_writer.get_ini_cst_string(cst)
-            self.assertEqual(result, out_str)
+                try:
+                    cst = ini_cst.get_cst(tokens)
+                except ini_cst.TooManyTabs as exception:
+                    if in_filepath.parent.name == "too_many_tabs":
+                        continue
+                    else:
+                        raise exception
+
+                ini_rules.apply_rules_on_sections([cst[0]], None)
+
+                result = ini_writer.get_ini_cst_string(cst)
+                self.assertEqual(result, out_str)
 
     def get_filepaths(self):
         return (
