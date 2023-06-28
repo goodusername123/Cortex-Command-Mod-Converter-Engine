bb3bc6d0a9f8107455101cf95237c92dfaee25fb
MyNameIsTrez <welfje@gmail.com>
2023-06-28 23:56:29 +0200

    Add pointer shenanigans test.zig back

--- test.zig
+++ test.zig
@@ -1,5 +1,42 @@
 const std = @import("std");
 
-fn foo() void {}
+fn move_ptr(line_ptr: *[*]const u8) void {
+    line_ptr.* += 1;
+}
 
-pub fn main() void {}
+fn move_slice(line_slice: *[]const u8) void {
+    line_slice.* = line_slice.*[1..];
+}
+
+pub fn main() void {
+    {
+        const line = [_]u8{ 'a', 'b', 'c' };
+        var line_ptr: [*]const u8 = &line;
+        line_ptr += 1;
+        std.log.debug("{c}", .{line_ptr[0]}); // b
+    }
+    {
+        var line = "abc";
+        var line_ptr: [*]const u8 = line;
+        line_ptr += 1;
+        std.log.debug("{c}", .{line_ptr[0]}); // b
+    }
+    {
+        var line = "abc";
+        var line_ptr: [*]const u8 = line;
+        move_ptr(&line_ptr);
+        std.log.debug("{c}", .{line_ptr[0]}); // b
+    }
+    {
+        const line = [_]u8{ 'a', 'b', 'c' };
+        var line_slice: []const u8 = &line;
+        line_slice = line_slice[1..];
+        std.log.debug("{c}", .{line_slice[0]}); // b
+    }
+    {
+        const line = [_]u8{ 'a', 'b', 'c' };
+        var line_slice: []const u8 = &line;
+        move_slice(&line_slice);
+        std.log.debug("{c}", .{line_slice[0]}); // b
+    }
+}
