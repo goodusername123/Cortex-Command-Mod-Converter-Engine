febfc6b80d8ed466219f58eb12f99b198b701c2e
MyNameIsTrez <welfje@gmail.com>
2023-06-29 01:10:33 +0200

    Add len test to test.zig

--- test.zig
+++ test.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const expect = std.testing.expect;
+
 fn move_ptr(line_ptr: *[*]const u8) void {
     line_ptr.* += 1;
 }
@@ -8,35 +10,42 @@ fn move_slice(line_slice: *[]const u8) void {
     line_slice.* = line_slice.*[1..];
 }
 
-pub fn main() void {
+pub fn main() !void {
     {
         const line = [_]u8{ 'a', 'b', 'c' };
         var line_ptr: [*]const u8 = &line;
         line_ptr += 1;
-        std.log.debug("{c}", .{line_ptr[0]}); // b
+        try expect(line_ptr[0] == 'b');
     }
     {
         var line = "abc";
         var line_ptr: [*]const u8 = line;
         line_ptr += 1;
-        std.log.debug("{c}", .{line_ptr[0]}); // b
+        try expect(line_ptr[0] == 'b');
     }
     {
         var line = "abc";
         var line_ptr: [*]const u8 = line;
         move_ptr(&line_ptr);
-        std.log.debug("{c}", .{line_ptr[0]}); // b
+        try expect(line_ptr[0] == 'b');
     }
     {
         const line = [_]u8{ 'a', 'b', 'c' };
         var line_slice: []const u8 = &line;
         line_slice = line_slice[1..];
-        std.log.debug("{c}", .{line_slice[0]}); // b
+        try expect(line_slice[0] == 'b');
     }
     {
         const line = [_]u8{ 'a', 'b', 'c' };
         var line_slice: []const u8 = &line;
         move_slice(&line_slice);
-        std.log.debug("{c}", .{line_slice[0]}); // b
+        try expect(line_slice[0] == 'b');
+    }
+    {
+        const line = [_]u8{ 'a', 'b', 'c' };
+        var line_slice: []const u8 = &line;
+        try expect(line_slice.len == 3);
+        move_slice(&line_slice);
+        try expect(line_slice.len == 2);
     }
 }
