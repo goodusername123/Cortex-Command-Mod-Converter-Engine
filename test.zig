d4ac6a56bf2dbaf76d318f1a122567e1b9fca931
MyNameIsTrez <welfje@gmail.com>
2023-06-28 04:05:50 +0200

    Clear test.zig

--- test.zig
+++ test.zig
@@ -1,28 +1,5 @@
 const std = @import("std");
 
-// pub fn main()
+fn foo() void {}
 
-fn move(line_ptr: *[*]const u8) void {
-    line_ptr.* += 1;
-}
-
-pub fn main() void {
-    {
-        const line = [_]u8{ 'a', 'b', 'c' };
-        var line_ptr: [*]const u8 = &line;
-        line_ptr += 1;
-        std.log.debug("{c}", .{line_ptr[0]}); // b
-    }
-    {
-        var line = "abc";
-        var line_ptr: [*]const u8 = line;
-        line_ptr += 1;
-        std.log.debug("{c}", .{line_ptr[0]}); // b
-    }
-    {
-        var line = "abc";
-        var line_ptr: [*]const u8 = line;
-        move(&line_ptr);
-        std.log.debug("{c}", .{line_ptr[0]}); // b
-    }
-}
+pub fn main() void {}
