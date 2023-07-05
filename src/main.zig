81e27b3f5beb18e57ee2bc71ede6ffc7bfef9afa
MyNameIsTrez <welfje@gmail.com>
2023-07-06 00:10:47 +0200

    Add getTokens()

--- src/main.zig
+++ src/main.zig
@@ -143,39 +143,172 @@ pub fn main() !void {
     defer allocator.free(lf_text);
     _ = replace(u8, text, "\r\n", "\n", lf_text);
 
-    var slice: []const u8 = lf_text;
+    // TODO: Should I stop passing the address of allocator, tokens, and ast everywhere?
 
-    var ast = try get_ast(&slice, &allocator);
+    var tokens = try getTokens(lf_text, &allocator);
+
+    var ast = try getAst(&tokens, &allocator);
+
+    // std.debug.print("{}\n", .{ast});
 
     const output_file = try cwd.createFile("src/output.ini", .{});
     defer output_file.close();
 
-    std.debug.print("{}\n", .{ast});
-
-    try write_ast(&ast, &output_file);
+    try writeAst(&ast, &output_file);
 
     // for (ast.children.items) |*child| {
-    //     try write_ast(child, &output_file);
+    //     try writeAst(child, &output_file);
     // }
 }
 
-fn get_ast(slice: *[]const u8, allocator: *Allocator) !Node {
+fn getTokens(lf_text: []const u8, allocator: *Allocator) ArrayList(Token) {
+    var slice: []const u8 = lf_text;
+
+    var tokens = ArrayList(Token).init(allocator.*);
+
+    var in_multiline_comment = false;
+
+    while (slice.len > 0) {
+        const token = getToken(slice, &in_multiline_comment);
+        tokens.append(token);
+
+        slice = slice[token.slice.len..];
+    }
+
+    return tokens;
+}
+
+fn getToken(slice: []const u8, in_multiline_comment: *bool) Token {
+    if (in_multiline_comment.*) {
+        var i: usize = 0;
+        while (i < slice.len) : (i += 1) {
+            if (slice[i] == '*' and i + 1 < slice.len and slice[i + 1] == '/') {
+                in_multiline_comment.* = false;
+                break;
+            }
+        }
+
+        const token = Token{ .type = .MultiComment, .slice = slice[0 .. i + 2] };
+        return token;
+    }
+
+    return switch (slice[0]) {
+        '/' => {
+            return switch (slice[1]) {
+                '/' => {
+                    const index = std.mem.indexOf(u8, slice[2..], "\n");
+                    const newline_index = if (index != null) index.? + 2 else slice.len;
+                    const token = Token{ .type = .SingleComment, .slice = slice[0..newline_index] };
+                    return token;
+                },
+                '*' => {
+                    in_multiline_comment.* = true;
+
+                    var i: usize = 2;
+                    // TODO: Either use while-loops or for-loops everywhere in this function
+                    while (i < slice.len) : (i += 1) {
+                        if (slice[i] == '*' and i + 1 < slice.len and slice[i + 1] == '/') {
+                            in_multiline_comment.* = false;
+                            i += 2;
+                            break;
+                        }
+                    }
+
+                    const token = Token{ .type = .MultiComment, .slice = slice[0..i] };
+                    return token;
+                },
+                else => {
+                    // std.debug.print("foo", .{});
+
+                    // A Sentence ends with a word, or the start of a comment
+                    var end_index: usize = 2;
+                    var sentence_end_index: usize = end_index;
+                    while (end_index < slice.len) : (end_index += 1) {
+                        if (slice[end_index] == '=' or slice[end_index] == '\n' or (slice[end_index] == '/' and end_index + 1 < slice.len and slice[end_index + 1] == '*')) {
+                            break;
+                        }
+                        if (slice[end_index] != ' ') {
+                            sentence_end_index = end_index + 1;
+                        }
+                    }
+
+                    const token = Token{ .type = .Sentence, .slice = slice[0..sentence_end_index] };
+                    return token;
+                },
+            };
+        },
+        '\t' => {
+            var i: usize = 1;
+            for (slice[1..]) |character| {
+                if (character != '\t') {
+                    break;
+                }
+                i += 1;
+            }
+
+            const token = Token{ .type = .Tabs, .slice = slice[0..i] };
+            return token;
+        },
+        ' ' => {
+            var i: usize = 1;
+            for (slice[1..]) |character| {
+                if (character != ' ') {
+                    break;
+                }
+                i += 1;
+            }
+
+            const token = Token{ .type = .Spaces, .slice = slice[0..i] };
+            return token;
+        },
+        '=' => {
+            const token = Token{ .type = .Equals, .slice = slice[0..1] };
+            // TODO: Check what happens if a line ends with an =, since I don't know if slice ends with '\0'
+            // TODO: The same question goes for the comment parsing code that reads 2 characters
+            return token;
+        },
+        '\n' => {
+            const token = Token{ .type = .Newline, .slice = slice[0..1] };
+            return token;
+        },
+        else => {
+            // std.debug.print("bar", .{});
+
+            // A Sentence ends with a word, or the start of a comment
+            var end_index: usize = 1;
+            var sentence_end_index: usize = end_index;
+            while (end_index < slice.len) : (end_index += 1) {
+                if (slice[end_index] == '=' or slice[end_index] == '\n' or (slice[end_index] == '/' and end_index + 1 < slice.len and slice[end_index + 1] == '*')) {
+                    break;
+                }
+                if (slice[end_index] != ' ') {
+                    sentence_end_index = end_index + 1;
+                }
+            }
+
+            const token = Token{ .type = .Sentence, .slice = slice[0..sentence_end_index] };
+            return token;
+        },
+    };
+}
+
+fn getAst(tokens: *ArrayList(Token), allocator: *Allocator) !Node {
     var ast = Node{
         .comments = ArrayList([]const u8).init(allocator.*),
         .children = ArrayList(Node).init(allocator.*),
     };
 
-    var in_multiline_comment = false;
+    var token_index: usize = 0;
 
-    while (slice.len > 0) {
-        const node = try get_node(slice, &in_multiline_comment, -1, allocator);
+    while (token_index < tokens.len) {
+        const node = try getNode(tokens, &token_index, 0, allocator);
         try ast.children.append(node);
     }
 
     return ast;
 }
 
-fn get_node(slice: *[]const u8, in_multiline_comment: *bool, depth: i32, allocator: *Allocator) !Node {
+fn getNode(tokens: *ArrayList(Token), token_index: *usize, depth: i32, allocator: *Allocator) !Node {
     const States = enum {
         Start,
         Property,
@@ -190,8 +323,8 @@ fn get_node(slice: *[]const u8, in_multiline_comment: *bool, depth: i32, allocat
         .children = ArrayList(Node).init(allocator.*),
     };
 
-    while (slice.len > 0) {
-        const token = getToken(slice, in_multiline_comment);
+    while (token_index < tokens.len) {
+        const token = tokens[token_index];
 
         // TODO: Figure out why {s: <42} doesn't set the width to 42
         std.debug.print("'{s}'\t\t{}\n", .{ fmtSliceEscapeUpper(token.slice), token.type });
@@ -199,23 +332,33 @@ fn get_node(slice: *[]const u8, in_multiline_comment: *bool, depth: i32, allocat
         if (seen == .Start and token.type == .Sentence) {
             node.property = token.slice;
             seen = .Property;
+            token_index += 1;
         } else if (seen == .Start and token.type == .Tabs) {
             node.tabs = token.slice;
+
             if (token.slice.len > depth) {
-                const child_node = try get_node(slice, in_multiline_comment, depth + 1, allocator);
+                const child_node = try getNode(tokens, token_index, depth + 1, allocator);
                 try node.children.append(child_node);
+            } else {
+                // TODO: This shouldn't be reached with the first encountered tab!
+                return node;
             }
         } else if (seen == .Property and token.type == .Equals) {
             seen = .Equals;
+            token_index += 1;
         } else if (seen == .Equals and token.type == .Sentence) {
             node.value = token.slice;
             seen = .Value;
+            token_index += 1;
         } else if (token.type == .SingleComment) {
             try node.comments.append(trim(u8, token.slice[2..], " "));
+            token_index += 1;
         } else if (token.type == .MultiComment) {
             try node.comments.append(trim(u8, token.slice[2 .. token.slice.len - 2], " "));
+            token_index += 1;
         } else if (token.type == .Spaces) {} else if (token.type == .Newline) {
             seen = .Start;
+            token_index += 1;
         } else {
             unreachable;
         }
@@ -224,130 +367,7 @@ fn get_node(slice: *[]const u8, in_multiline_comment: *bool, depth: i32, allocat
     return node;
 }
 
-fn getToken(slice: *[]const u8, in_multiline_comment: *bool) Token {
-    if (in_multiline_comment.*) {
-        var i: usize = 0;
-        while (i < slice.len) : (i += 1) {
-            if (slice.*[i] == '*' and i + 1 < slice.len and slice.*[i + 1] == '/') {
-                in_multiline_comment.* = false;
-                break;
-            }
-        }
-
-        const token = Token{ .type = .MultiComment, .slice = slice.*[0 .. i + 2] };
-        slice.* = slice.*[i + 2 ..];
-        return token;
-    }
-
-    return switch (slice.*[0]) {
-        '/' => {
-            return switch (slice.*[1]) {
-                '/' => {
-                    const index = std.mem.indexOf(u8, slice.*[2..], "\n");
-                    const newline_index = if (index != null) index.? + 2 else slice.len;
-                    const token = Token{ .type = .SingleComment, .slice = slice.*[0..newline_index] };
-                    slice.* = slice.*[newline_index..];
-                    return token;
-                },
-                '*' => {
-                    in_multiline_comment.* = true;
-
-                    var i: usize = 2;
-                    // TODO: Either use while-loops or for-loops everywhere in this function
-                    while (i < slice.len) : (i += 1) {
-                        if (slice.*[i] == '*' and i + 1 < slice.len and slice.*[i + 1] == '/') {
-                            in_multiline_comment.* = false;
-                            i += 2;
-                            break;
-                        }
-                    }
-
-                    const token = Token{ .type = .MultiComment, .slice = slice.*[0..i] };
-                    slice.* = slice.*[i..];
-                    return token;
-                },
-                else => {
-                    // std.debug.print("foo", .{});
-
-                    // A Sentence ends with a word, or the start of a comment
-                    var end_index: usize = 2;
-                    var sentence_end_index: usize = end_index;
-                    while (end_index < slice.len) : (end_index += 1) {
-                        if (slice.*[end_index] == '=' or slice.*[end_index] == '\n' or (slice.*[end_index] == '/' and end_index + 1 < slice.len and slice.*[end_index + 1] == '*')) {
-                            break;
-                        }
-                        if (slice.*[end_index] != ' ') {
-                            sentence_end_index = end_index + 1;
-                        }
-                    }
-
-                    const token = Token{ .type = .Sentence, .slice = slice.*[0..sentence_end_index] };
-                    slice.* = slice.*[sentence_end_index..];
-                    return token;
-                },
-            };
-        },
-        '\t' => {
-            var i: usize = 1;
-            for (slice.*[1..]) |character| {
-                if (character != '\t') {
-                    break;
-                }
-                i += 1;
-            }
-
-            const token = Token{ .type = .Tabs, .slice = slice.*[0..i] };
-            slice.* = slice.*[i..];
-            return token;
-        },
-        ' ' => {
-            var i: usize = 1;
-            for (slice.*[1..]) |character| {
-                if (character != ' ') {
-                    break;
-                }
-                i += 1;
-            }
-
-            const token = Token{ .type = .Spaces, .slice = slice.*[0..i] };
-            slice.* = slice.*[i..];
-            return token;
-        },
-        '=' => {
-            const token = Token{ .type = .Equals, .slice = slice.*[0..1] };
-            // TODO: Check what happens if a line ends with an =, since I don't know if slice ends with '\0'
-            // TODO: The same question goes for the comment parsing code that reads 2 characters
-            slice.* = slice.*[1..];
-            return token;
-        },
-        '\n' => {
-            const token = Token{ .type = .Newline, .slice = slice.*[0..1] };
-            slice.* = slice.*[1..];
-            return token;
-        },
-        else => {
-            // std.debug.print("bar", .{});
-
-            // A Sentence ends with a word, or the start of a comment
-            var end_index: usize = 1;
-            var sentence_end_index: usize = end_index;
-            while (end_index < slice.len) : (end_index += 1) {
-                if (slice.*[end_index] == '=' or slice.*[end_index] == '\n' or (slice.*[end_index] == '/' and end_index + 1 < slice.len and slice.*[end_index + 1] == '*')) {
-                    break;
-                }
-                if (slice.*[end_index] != ' ') {
-                    sentence_end_index = end_index + 1;
-                }
-            }
-
-            const token = Token{ .type = .Sentence, .slice = slice.*[0..sentence_end_index] };
-            slice.* = slice.*[sentence_end_index..];
-            return token;
-        },
-    };
-}
-
-fn write_ast(node: *Node, file: *const std.fs.File) !void {
+fn writeAst(node: *Node, file: *const std.fs.File) !void {
     // Write tabs to file
     if (node.tabs != null) {
         std.debug.print("'{s}'\n", .{fmtSliceEscapeUpper(node.tabs.?)});
@@ -387,7 +407,7 @@ fn write_ast(node: *Node, file: *const std.fs.File) !void {
     // Recursively enter child nodes
     std.debug.print("Recursing into child\n", .{});
     for (node.children.items) |*child| {
-        try write_ast(child, file);
+        try writeAst(child, file);
     }
 
     // Print nodes
