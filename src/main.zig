20c189991ab06a9cc662fe16e05df663a20032b0
MyNameIsTrez <welfje@gmail.com>
2023-07-30 18:29:59 +0200

    Don't look for in/mods/ subdirectory

--- src/main.zig
+++ src/main.zig
@@ -124,7 +124,7 @@ pub fn main() !void {
     const cwd = std.fs.cwd();
 
     var input_mod_path_buffer: [MAX_PATH_BYTES]u8 = undefined;
-    const input_mod_path = try cwd.realpath("tons_of_mods/in/mods", &input_mod_path_buffer);
+    const input_mod_path = try cwd.realpath("tons_of_mods/in", &input_mod_path_buffer);
 
     var output_mod_path_buffer: [MAX_PATH_BYTES]u8 = undefined;
     const output_mod_path = try cwd.realpath("tons_of_mods/out", &output_mod_path_buffer);
